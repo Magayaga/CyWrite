0087f7564419e528b81719a2f4b5940363edce4f
cyrilmagayaga <cyrilmagayaga@gmail.com>
2023-08-13 18:18:27 +0800

    update release (v1.0-preview2)

--- src/cywrite/CyWrite.java
+++ src/cywrite/CyWrite.java
@@ -1,243 +1,224 @@
-package cywrite;
-
-/**
- *
- * CYWRITE - SIMPLE TEXT EDITOR
- * @author Cyril John Magayaga
- * @latestReleaseDate February 2, 2023
- * @latestVersionNumber v1.0-preview1
- * 
- */
 import java.awt.*;
 import javax.swing.*;
 import java.io.*;
 import java.awt.event.*;
 import javax.swing.plaf.metal.*;
+import javax.swing.undo.UndoManager;
+import javax.swing.undo.UndoableEdit;
+import javax.swing.undo.CannotUndoException;
+import javax.swing.undo.CannotRedoException;
 import javax.swing.text.*;
-/**
- *
- * CYWRITE - SIMPLE TEXT EDITOR
- * @author Cyril John Magayaga
- * @latestReleaseDate August 20, 2022
- * @latestVersionNumber v1.0-preview0
- * 
- */
-public class CyWrite 
-        extends JFrame 
-        implements ActionListener {
-	// Text component
-	JTextArea t;
+import javax.swing.tree.*;
 
-	// Frame
-	JFrame f;
+public class CyWrite extends JFrame implements ActionListener {
+    // Text component
+    JTextArea t;
+	private UndoManager undoManager;
+	float fontSize = 12.0f;
+    // Constructor
+    CyWrite() {
+        // Create a frame
+        super("CyWrite"); // Set frame title
 
-	// Constructor
-	CyWrite(){
-		// Create a frame
-		f = new JFrame("CyWrite");
+        // Text component
+        t = new JTextArea();
+		undoManager = new UndoManager();
+        t.getDocument().addUndoableEditListener(undoManager);
 
-		try {
-			// Set metal look and feel
-			UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+        // Create a menubar
+        JMenuBar mb = new JMenuBar();
 
-			// Set theme to ocean
-			MetalLookAndFeel.setCurrentTheme(new OceanTheme());
-		}
-		catch (Exception e) {
-		}
-
-		// Text component
-		t = new JTextArea();
-
-		// Create a menubar
-		JMenuBar mb = new JMenuBar();
-
-		// Create amenu for menu
-		JMenu m1 = new JMenu("File");
-
-		// Create menu items
-		JMenuItem mi1 = new JMenuItem("New");
-		JMenuItem mi2 = new JMenuItem("Open");
-		JMenuItem mi3 = new JMenuItem("Save");
-		JMenuItem mi9 = new JMenuItem("Print");
-                JMenuItem mi10 = new JMenuItem("Exit");
-
-		// Add action listener
-		mi1.addActionListener(this);
-		mi2.addActionListener(this);
-		mi3.addActionListener(this);
-		mi9.addActionListener(this);
-                mi10.addActionListener(this);
-
-		m1.add(mi1);
-		m1.add(mi2);
-		m1.add(mi3);
-		m1.add(mi9);
-                m1.add(mi10);
-
-		// Create amenu for menu
+        // Create a menu for File
+        JMenu m1 = new JMenu("File");
 		JMenu m2 = new JMenu("Edit");
+        JMenu m3 = new JMenu("View");
+		JMenu m4 = new JMenu("Help");
 
-		// Create menu items
-		JMenuItem mi4 = new JMenuItem("Cut");
-		JMenuItem mi5 = new JMenuItem("Copy");
-		JMenuItem mi6 = new JMenuItem("Paste");
+        // Create menu items
+        JMenuItem mi1 = new JMenuItem("New");
+        JMenuItem mi2 = new JMenuItem("Open");
+        JMenuItem mi3 = new JMenuItem("Save");
+        JMenuItem mi9 = new JMenuItem("Print");
+        JMenuItem mi10 = new JMenuItem("Exit");
 
-		// Add action listener
-		mi4.addActionListener(this);
-		mi5.addActionListener(this);
-		mi6.addActionListener(this);
-                
-		m2.add(mi4);
-		m2.add(mi5);
-		m2.add(mi6);
-                
-                // Create amenu for menu
-                JMenu m3 = new JMenu("Help");
-                
-                JMenuItem mi17 = new JMenuItem("About");
-                
-                // Add action listener
-                mi17.addActionListener(this);
-                
-                m3.add(mi17);
-                
-		mb.add(m1);
-		mb.add(m2);
-                mb.add(m3);
+		JMenuItem mi18 = new JMenuItem("Undo");
+		JMenuItem mi19 = new JMenuItem("Redo");
+        JMenuItem mi4 = new JMenuItem("Cut");
+        JMenuItem mi5 = new JMenuItem("Copy");
+        JMenuItem mi6 = new JMenuItem("Paste");
 
-		f.setJMenuBar(mb);
-		f.add(t);
-		f.setSize(800, 575);
-		f.show();
-	}
+		JMenuItem mi20 = new JMenuItem("Zoom In");
+		JMenuItem mi21 = new JMenuItem("Zoom Out");
 
-/**
- *
- * CYWRITE - SIMPLE TEXT EDITOR
- * @author Cyril John Magayaga
- * @latestReleaseDate August 20, 2022
- * @latestVersionNumber v1.0-preview0
- * 
- */
-        
-	// If a button is pressed
-	public void actionPerformed(ActionEvent e){
-		String s = e.getActionCommand();
+		JMenuItem mi17 = new JMenuItem("About");
 
-		if (s.equals("Cut")) {
-			t.cut();
-		}
+        // Add action listener
+        mi1.addActionListener(this);
+        mi2.addActionListener(this);
+        mi3.addActionListener(this);
+        mi9.addActionListener(this);
+        mi10.addActionListener(this);
+
+        // Add action listener
+		mi18.addActionListener(this);
+		mi19.addActionListener(this);
+        mi4.addActionListener(this);
+        mi5.addActionListener(this);
+        mi6.addActionListener(this);
+
+		mi20.addActionListener(this);
+		mi21.addActionListener(this);
+
+        // Add action listener
+        mi17.addActionListener(this);
+
+        m1.add(mi1);
+        m1.add(mi2);
+        m1.add(mi3);
+        m1.add(mi9);
+        m1.add(mi10);
+
+		m2.add(mi18);
+		m2.add(mi19);
+        m2.add(mi4);
+        m2.add(mi5);
+        m2.add(mi6);
+
+		m3.add(mi20);
+		m3.add(mi21);
+
+        m4.add(mi17);
+
+        mb.add(m1);
+        mb.add(m2);
+		mb.add(m3);
+        mb.add(m4);
+
+        setJMenuBar(mb);
+        add(t);
+        setSize(800, 575);
+        setVisible(true);
+    }
+
+    // If a button is pressed
+    public void actionPerformed(ActionEvent e) {
+        String s = e.getActionCommand();
+
+        if (s.equals("Cut")) {
+            t.cut();
+        }
+		
 		else if (s.equals("Copy")) {
-			t.copy();
-		}
+            t.copy();
+        }
+		
 		else if (s.equals("Paste")) {
-			t.paste();
-		}
+            t.paste();
+        }
+		
 		else if (s.equals("Save")) {
-			// Create an object of JFileChooser class
-			JFileChooser j = new JFileChooser("f:");
+            JFileChooser j = new JFileChooser("f:");
 
-			// Invoke the showsSaveDialog function to show the save dialog
-			int r = j.showSaveDialog(null);
+            int r = j.showSaveDialog(null);
 
-			if (r == JFileChooser.APPROVE_OPTION) {
+            if (r == JFileChooser.APPROVE_OPTION) {
+                File fi = new File(j.getSelectedFile().getAbsolutePath());
 
-				// Set the label to the path of the selected directory
-				File fi = new File(j.getSelectedFile().getAbsolutePath());
+                try {
+                    FileWriter wr = new FileWriter(fi, false);
 
-				try {
-					// Create a file writer
-					FileWriter wr = new FileWriter(fi, false);
+                    BufferedWriter w = new BufferedWriter(wr);
 
-					// Create buffered writer to write
-					BufferedWriter w = new BufferedWriter(wr);
+                    w.write(t.getText());
 
-					// Write
-					w.write(t.getText());
-
-					w.flush();
-					w.close();
-				}
-				catch (Exception evt) {
-					JOptionPane.showMessageDialog(f, evt.getMessage());
-				}
-			}
-			// If the user cancelled the operation
-			else
-				JOptionPane.showMessageDialog(f, "the user cancelled the operation");
-		}
+                    w.flush();
+                    w.close();
+                } catch (Exception evt) {
+                    JOptionPane.showMessageDialog(this, evt.getMessage());
+                }
+            } else
+                JOptionPane.showMessageDialog(this, "The user cancelled the operation");
+        }
+		
 		else if (s.equals("Print")) {
-			try {
-				// print the file
-				t.print();
-			}
-			catch (Exception evt) {
-				JOptionPane.showMessageDialog(f, evt.getMessage());
-			}
-		}
+            try {
+                t.print();
+            } catch (Exception evt) {
+                JOptionPane.showMessageDialog(this, evt.getMessage());
+            }
+        }
+		
 		else if (s.equals("Open")) {
-			// Create an object of JFileChooser class
-			JFileChooser j = new JFileChooser("f:");
+            JFileChooser j = new JFileChooser("f:");
 
-			// Invoke the showsOpenDialog function to show the save dialog
-			int r = j.showOpenDialog(null);
+            int r = j.showOpenDialog(null);
 
-			// If the user selects a file
-			if (r == JFileChooser.APPROVE_OPTION) {
-				// Set the label to the path of the selected directory
-				File fi = new File(j.getSelectedFile().getAbsolutePath());
+            if (r == JFileChooser.APPROVE_OPTION) {
+                File fi = new File(j.getSelectedFile().getAbsolutePath());
 
-				try {
-					// String
-					String s1 = "", sl = "";
+                try {
+                    String s1 = "", sl = "";
+                    FileReader fr = new FileReader(fi);
+                    BufferedReader br = new BufferedReader(fr);
 
-					// File reader
-					FileReader fr = new FileReader(fi);
+                    sl = br.readLine();
 
-					// Buffered reader
-					BufferedReader br = new BufferedReader(fr);
+                    while ((s1 = br.readLine()) != null) {
+                        sl = sl + "\n" + s1;
+                    }
 
-					// Initialize sl
-					sl = br.readLine();
-
-					// Take the input from the file
-					while ((s1 = br.readLine()) != null) {
-						sl = sl + "\n" + s1;
-					}
-
-					// Set the text
-					t.setText(sl);
-				}
-				catch (Exception evt) {
-					JOptionPane.showMessageDialog(f, evt.getMessage());
-				}
-			}
-			// If the user cancelled the operation
-			else
-				JOptionPane.showMessageDialog(f, "the user cancelled the operation");
-		}
+                    t.setText(sl);
+                } catch (Exception evt) {
+                    JOptionPane.showMessageDialog(this, evt.getMessage());
+                }
+            } else
+                JOptionPane.showMessageDialog(this, "The user cancelled the operation");
+        }
+		
 		else if (s.equals("New")) {
-			t.setText("");
-		}
+            t.setText("");
+        }
+		
 		else if (s.equals("Exit")) {
-			f.setVisible(false);
-		}
-                else if (s.equals("About")) {
-			JOptionPane.showMessageDialog(f, "CyWrite is simple text editor created and developed by Cyril John Magayaga.\n v1.0 was current first released on September 5, 2022.");
-		}
-	}
+            setVisible(false);
+        }
+		
+		else if (s.equals("About")) {
+            JOptionPane.showMessageDialog(this,
+                    "CyWrite is a simple text editor created and developed by Cyril John Magayaga.\n"
+                            + "v1.0 was first released on September 5, 2022.");
+        }
+		
+		else if (s.equals("Undo")) {
+            try {
+                if (undoManager.canUndo()) {
+                    undoManager.undo();
+                }
+            } catch (CannotUndoException cue) {
+            }
+        }
 
-    /**
-     *
-     * 	Proprietary software,
-     *  CyWrite. - Simple Text Editor created and developed by Cyril John Magayaga.
-     * 
-     *  It is written in Java programming language, like Microsoft Notepad, 
-     *  Microsoft WordPad, GNU Emacs, Apple TextEdit, Vim, and more text editors.
-     */
-	// Main class
-	public static void main(String args[]){
-		CyWrite e = new CyWrite();
-	}
+		else if (s.equals("Redo")) {
+            try {
+                if (undoManager.canRedo()) {
+                    undoManager.redo();
+                }
+            } catch (CannotRedoException cre) {
+            }
+        }
+
+		else if (s.equals("Zoom In")) {
+            fontSize += 1.0f;
+            t.setFont(t.getFont().deriveFont(fontSize));
+        }
+
+        else if (s.equals("Zoom Out")) {
+            fontSize -= 1.0f;
+            t.setFont(t.getFont().deriveFont(fontSize));
+        }
+    }
+
+    public static void main(String args[]) {
+        new CyWrite();
+    }
 }
